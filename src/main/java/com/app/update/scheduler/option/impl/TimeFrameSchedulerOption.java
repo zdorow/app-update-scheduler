b0bd747bba6836702c72dff1015ca128a0ac0566
Zach Dorow <zdorow@me.com>
2018-07-30 22:43:23 -0500

    Formatting changes

--- src/main/java/com/app/update/scheduler/option/impl/TimeFrameSchedulerOption.java
+++ src/main/java/com/app/update/scheduler/option/impl/TimeFrameSchedulerOption.java
@@ -11,92 +11,83 @@ import javafx.scene.text.Text;
 
 public class TimeFrameSchedulerOption extends Task<Boolean> {
 
-	private static final String updateXml = "<mobile_device_application><general><itunes_sync_time>%d</itunes_sync_time></general></mobile_device_application>";
+    private static final String updateXml = "<mobile_device_application><general><itunes_sync_time>%d</itunes_sync_time></general></mobile_device_application>";
 
-	private final JssApi jssApi;
-	private final List<Integer> appIdList;
-	private final Text actiontarget;
-	private final TimeFrame timeFrameStart;
-	private final TimeFrame timeFrameEnd;
+    private final JssApi jssApi;
+    private final List<Integer> appIdList;
+    private final Text actiontarget;
+    private final TimeFrame timeFrameStart;
+    private final TimeFrame timeFrameEnd;
 
-	public TimeFrameSchedulerOption(JssApi jssApi, List<Integer> appIdList, Text actiontarget, TimeFrame timeFrameStart, TimeFrame timeFrameEnd) {
-		this.jssApi = jssApi;
-		this.appIdList = appIdList;
-		this.actiontarget = actiontarget;
-		this.timeFrameStart = timeFrameStart;
-		this.timeFrameEnd = timeFrameEnd;
-	}
+    public TimeFrameSchedulerOption(JssApi jssApi, List<Integer> appIdList, Text actiontarget, TimeFrame timeFrameStart, TimeFrame timeFrameEnd) {
+        this.jssApi = jssApi;
+        this.appIdList = appIdList;
+        this.actiontarget = actiontarget;
+        this.timeFrameStart = timeFrameStart;
+        this.timeFrameEnd = timeFrameEnd;
+    }
 
-	@Override
-	protected Boolean call() throws Exception {
+    @Override
+    protected Boolean call() throws Exception {
 
-		actiontarget.setText("Calculating spread of application updates");
+        actiontarget.setText("Calculating spread of application updates");
 
-		try {
-			int count = 0;
-			double spreadBeforeMidnight, spreadAfterMidnight;
-			double startTime = timeFrameStart.calculateNumberOfSecondsFromMidnight();
-			double endTime = timeFrameEnd.calculateNumberOfSecondsFromMidnight();
-			actiontarget.setText("Updating application update schedules");
-			
-			if(endTime < startTime)
-			{
-			System.out.println("Endtime less than start detected. Running compensation sequence");
-			double midnight = 0.0;
-                        double timeUntilMidnight = 86400 - startTime;
-                        double calcualtePercentageList = timeUntilMidnight / (startTime + endTime);
-                        System.out.println("Percentage of list to be allocated before midnight" + calcualtePercentageList * 100);
-			double percentListTotal=(Math.round(appIdList.size() * calcualtePercentageList));
+        try {
+            int count = 0;
+            double spreadBeforeMidnight, spreadAfterMidnight;
+            double startTime = timeFrameStart.calculateNumberOfSecondsFromMidnight();
+            double endTime = timeFrameEnd.calculateNumberOfSecondsFromMidnight();
+            actiontarget.setText("Updating application update schedules");
 
-			List<Integer> appIdListBeforeMidnight = appIdList.subList(0, (int)percentListTotal);
-			List<Integer> appIdListAfterMidnight = appIdList.subList((int)percentListTotal, appIdList.size());
-			
-			spreadBeforeMidnight = timeUntilMidnight / new Double(appIdListBeforeMidnight.size());
-			spreadAfterMidnight = endTime / new Double(appIdListAfterMidnight.size());
-			
-			for (int id : appIdListBeforeMidnight) {
-				jssApi.put("mobiledeviceapplications/id/" + id, String.format(updateXml, Math.round(startTime)));
-                                System.out.println("Spread Before midnight:" + spreadBeforeMidnight);
-                                System.out.println("Start Time:" + startTime);
-				updateProgress(count, appIdListBeforeMidnight.size());
-				count++;
-				startTime += spreadBeforeMidnight;
-				Double percent = (double) count/appIdListBeforeMidnight.size()*100;
-				actiontarget.setText("Updating applications that run before midnight. " + percent.intValue() + "%");
-			}
-			count=0;
-			for (int id : appIdListAfterMidnight) {
-				jssApi.put("mobiledeviceapplications/id/" + id, String.format(updateXml, Math.round(midnight)));
+            if (endTime < startTime) {
+                // In this sequence we use the percentage of total time before midnight to allocate which apps 
+                // will be updated before midnight. The percentage of time is based on the total time selected.
+                System.out.println("Endtime less than start detected. Running compensation sequence");
+                double midnight = 0.0;
+                double timeUntilMidnight = 86400 - startTime;
+                double calcualtePercentageList = timeUntilMidnight / (startTime + endTime);
+                System.out.println("Percentage of list to be allocated before midnight" + calcualtePercentageList * 100);
+                double percentListTotal = (Math.round(appIdList.size() * calcualtePercentageList));
 
-				updateProgress(count, appIdListAfterMidnight.size());
-				count++;
-				midnight += spreadAfterMidnight;
-				Double percent = (double) count/appIdListAfterMidnight.size()*100;
-				actiontarget.setText("Updating applications that run after midnight. " + percent.intValue() + "%");
-			}
-			}
-			
-			else {
-			double spread = endTime / new Double(appIdList.size());
-			
-			for (int id : appIdList) {
-				jssApi.put("mobiledeviceapplications/id/" + id, String.format(updateXml, Math.round(startTime)));
+                // This is where the two lists get broken up
+                List<Integer> appIdListBeforeMidnight = appIdList.subList(0, (int) percentListTotal);
+                List<Integer> appIdListAfterMidnight = appIdList.subList((int) percentListTotal, appIdList.size());
 
-				updateProgress(count, appIdList.size());
-				count++;
-				startTime += spread;
-				Double percent = (double) count/appIdList.size()*100;
-				actiontarget.setText(percent.intValue() + "%");
-			}
-		}
-			updateProgress(1, 1);
-			appIdList.clear();
-		} catch (JssApiException e) {
-			actiontarget.setText("There was an error while processing app updates.");
-			return false;
-		}
+                //This is where the spread gets calculated.
+                spreadBeforeMidnight = timeUntilMidnight / new Double(appIdListBeforeMidnight.size());
+                spreadAfterMidnight = endTime / new Double(appIdListAfterMidnight.size());
 
-		return true;
-	}
-        
+                for (int id : appIdListBeforeMidnight) {
+                    setSyncTime(id, startTime, count, spreadBeforeMidnight, appIdListBeforeMidnight,
+                            "Setting iTunes sync time for apps that run before midnight: ");
+                }
+                count = 0;
+                for (int id : appIdListAfterMidnight) {
+                    setSyncTime(id, midnight, count, spreadAfterMidnight, appIdListAfterMidnight,
+                            "Setting iTunes sync time for apps that run after midnight: ");
+                }
+            } else {
+                double spread = endTime / new Double(appIdList.size());
+
+                for (int id : appIdList) {
+                    setSyncTime(id, startTime, count, spread, appIdList, "Setting iTunes sync time: ");
+                }
+            }
+            updateProgress(1, 1);
+            appIdList.clear();
+        } catch (JssApiException e) {
+            actiontarget.setText("There was an error while processing app updates.");
+            return false;
+        }
+        return true;
+    }
+
+    public void setSyncTime(int id, double time, int count, double spread, List<Integer> idList, String messageText) throws JssApiException {
+        jssApi.put("mobiledeviceapplications/id/" + id, String.format(updateXml, Math.round(time)));
+        updateProgress(count, idList.size());
+        count++;
+        time += spread;
+        Double percent = (double) count / idList.size() * 100;
+        actiontarget.setText(messageText + percent.intValue() + "%");
+    }
 }
